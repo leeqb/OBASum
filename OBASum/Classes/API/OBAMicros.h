dfe0f91370914035cdf2b0a3ab6da6e3d10e6439
xboy <dolphin2525@163.com>
2018-07-12 00:46:24 +0800

    test111

--- OBASum/Classes/API/OBAMicros.h
+++ OBASum/Classes/API/OBAMicros.h
@@ -11,6 +11,7 @@
 
 #define SCREEN_WIDTH    ([[UIScreen mainScreen] bounds].size.width) // 屏幕宽度
 #define SCREEN_HEIGHT   ([[UIScreen mainScreen] bounds].size.height)    // 屏幕高度
+#define TEST 111
 
 // 颜色
 #define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
